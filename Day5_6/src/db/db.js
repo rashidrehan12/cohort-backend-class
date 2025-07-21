ec98d02ac004a0b677f60b4f120e56002fa171d4
Md Rashid <118841846+rashidrehan12@users.noreply.github.com>
2025-07-21 17:39:41 +0530

    update mongoDB URL

--- Day5_6/src/db/db.js
+++ Day5_6/src/db/db.js
@@ -5,7 +5,7 @@ const moongoose = require('mongoose'); // Import the mongoose library to interac
 // server Database se kaise connect hoga ye tum db.js file me likhoge
 
 function connectDB() { // Define a function to connect to the database
-    moongoose.connect("mongodb+srv://rashidrehan:kxOq7JPbgJmUdgFp@cluster0.8zvgf4b.mongodb.net/cohort") // Use mongoose to connect to the MongoDB database
+    moongoose.connect("") // Use mongoose to connect to the MongoDB database
     .then(() => { // If the connection is successful, execute this block
         console.log("Database connected successfully"); // Log a success message to the console
     })
